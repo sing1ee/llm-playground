b0c144509460040ef8cc11cebf2bb22a7042691e
zhangcheng <zh.milo@gmail.com>
2024-09-29 15:37:35 +0800

    svg component

--- src/app/components/PlaygroundForm.tsx
+++ src/app/components/PlaygroundForm.tsx
@@ -34,6 +34,7 @@ import { cyberpunk } from '@codesandbox/sandpack-themes';
 import { handleDownload } from '../lib/download';
 import { CopyIcon, DownloadIcon, PlayIcon } from '@radix-ui/react-icons';
 import IconTrash from './IconTrash';
+import IconAdd from './IconAdd';
 
 export default function PlaygroundForm({ setResult }: PlaygroundFormProps) {
   const [prompt, setPrompt] = useState('');
@@ -330,20 +331,7 @@ export default function PlaygroundForm({ setResult }: PlaygroundFormProps) {
                 aria-label="Add new role"
                 title="Add new role"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
+                <IconAdd />
               </Button>
             </PopoverTrigger>
             <PopoverContent className="w-80 bg-white">
